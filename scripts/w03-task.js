c8acc9c57de0bfeec9531bdb930ae75a792958ed
Samuel Pletain <samuel.pletain@gmail.com>
2024-03-16 09:13:35 -0600

    Removed coma

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -2,11 +2,11 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
-function add(number1, number2){
+function add(number1, number2) {
   return number1 + number2;
 }
 
-function addNumbers(){
+function addNumbers() {
   const number1 = Number(document.getElementById('add1').value);
   const number2 = Number(document.getElementById('add2').value);
   document.getElementById('sum').value = add(number1, number2);
@@ -16,15 +16,15 @@ const addNumbersButton = document.getElementById('addNumbers');
 addNumbersButton.addEventListener('click', addNumbers);
 
 /* Function Expression - Subtract Numbers */
-const subtract = function(number1, number2){
+const subtract = function (number1, number2) {
   return number1 - number2;
-}
+};
 
-const subtractNumbers = function(){
+const subtractNumbers = function () {
   const number1 = Number(document.getElementById('subtract1').value);
   const number2 = Number(document.getElementById('subtract2').value);
   document.getElementById('difference').value = subtract(number1, number2);
-}
+};
 
 const subtractNumbersButton = document.getElementById('subtractNumbers');
 subtractNumbersButton.addEventListener('click', subtractNumbers);
@@ -36,7 +36,7 @@ const multiplyNumbers = () => {
   const number1 = Number(document.getElementById('factor1').value);
   const number2 = Number(document.getElementById('factor2').value);
   document.getElementById('product').value = multiply(number1, number2);
-}
+};
 
 const multiplyNumbersButton = document.getElementById('multiplyNumbers');
 multiplyNumbersButton.addEventListener('click', multiplyNumbers);
@@ -48,20 +48,20 @@ const divideNumbers = () => {
   const number1 = Number(document.getElementById('dividend').value);
   const number2 = Number(document.getElementById('divisor').value);
   document.getElementById('quotient').value = divide(number1, number2);
-}
+};
 
 const divideNumbersButton = document.getElementById('divideNumbers');
 divideNumbersButton.addEventListener('click', divideNumbers);
 
 /* Decision Structure */
-function getTotalDue(){
+function getTotalDue() {
   const subtotal = Number(document.getElementById('subtotal').value);
   const member = document.getElementById('member').checked;
-  
-  const discount = member ? subtotal * .8 : subtotal;
-  
+
+  const discount = member ? subtotal * 0.8 : subtotal;
+
   const span = document.getElementById('total');
-  span.textContent = `$ ${discount.toFixed(2)}`;
+  span.textContent = `$${discount.toFixed(2)}`;
 }
 
 const getTotalDueButton = document.getElementById('getTotal');
@@ -75,12 +75,12 @@ const arraySpan = document.getElementById('array');
 arraySpan.textContent = numbersArray;
 
 /* Output Odds Only Array */
-const oddsArray = numbersArray.filter(number => number % 2 !== 0);
+const oddsArray = numbersArray.filter((number) => number % 2 !== 0);
 const oddsSpan = document.getElementById('odds');
 oddsSpan.textContent = oddsArray;
 
 /* Output Evens Only Array */
-const evensArray = numbersArray.filter(number => number % 2 === 0);
+const evensArray = numbersArray.filter((number) => number % 2 === 0);
 const evensSpan = document.getElementById('evens');
 evensSpan.textContent = evensArray;
 
@@ -90,11 +90,14 @@ const sumSpan = document.getElementById('sumOfArray');
 sumSpan.textContent = sum;
 
 /* Output Multiplied by 2 Array */
-const multipliedArray = numbersArray.map(number => number * 2);
+const multipliedArray = numbersArray.map((number) => number * 2);
 const multipliedSpan = document.getElementById('multiplied');
 multipliedSpan.textContent = multipliedArray;
 
 /* Output Sum of Multiplied by 2 Array */
-const multipliedSum = multipliedArray.reduce((total, number) => total + number, 0);
+const multipliedSum = multipliedArray.reduce(
+  (total, number) => total + number,
+  0
+);
 const multipliedSumSpan = document.getElementById('sumOfMultiplied');
 multipliedSumSpan.textContent = multipliedSum;
