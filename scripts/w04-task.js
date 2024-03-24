84cc60ed9265e47993490ef0c5e1c0f55cedaba3
Samuel Pletain <samuel.pletain@gmail.com>
2024-03-23 20:56:23 -0600

    Improvements

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -2,57 +2,67 @@
 
 /* Profile Object  */
 const myProfile = {
-  name: "Samuel Pletain",
-  photo: "images/samuel-pletain.jpeg",
+  name: 'Samuel Pletain',
+  photo: 'images/samuel-pletain.jpeg',
   favoriteFoods: ['Sushi', 'Pasta', 'Burgers'],
   hobbies: ['Reading', 'Board Games', 'Coding'],
-  placesLived: []
+  placesLived: [],
 };
 
-myProfile.placesLived.push({
-  place: 'Nice',
-  length: '23 years'
-});
-myProfile.placesLived.push({
-  place: 'Paris',
-  length: '2 years'
-});
-myProfile.placesLived.push({
-  place: 'St Quentin',
-  length: '1 years'
-});
-myProfile.placesLived.push({
-  place: 'Torcy',
-  length: '7 years'
-});
-myProfile.placesLived.push({
-  place: 'Midvale',
-  length: '4 years'
-});
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+  {
+    place: 'Nice',
+    length: '23 years',
+  },
+  {
+    place: 'Paris',
+    length: '2 years',
+  },
+  {
+    place: 'St Quentin',
+    length: '1 years',
+  },
+  {
+    place: 'Torcy',
+    length: '7 years',
+  },
+  {
+    place: 'Midvale',
+    length: '4 years',
+  }
+);
 
+/* DOM Manipulation - Output */
+
+/* Name */
 const nameElement = document.getElementById('name');
 nameElement.innerHTML = `<strong>${myProfile.name}</strong>`;
 
+/* Photo with attributes */
 const imageElement = document.getElementById('photo');
 imageElement.setAttribute('src', myProfile.photo);
 imageElement.setAttribute('alt', `Profile picture of ${myProfile.name}`);
 
+/* Favorite Foods List*/
 const foodElement = document.getElementById('favorite-foods');
-myProfile.favoriteFoods.forEach(food => {
+myProfile.favoriteFoods.forEach((food) => {
   const listItemElement = document.createElement('li');
   listItemElement.innerHTML = food;
   foodElement.appendChild(listItemElement);
 });
 
+/* Hobbies List */
 const hobbiesElement = document.getElementById('hobbies');
-myProfile.hobbies.forEach(hobby => {
+myProfile.hobbies.forEach((hobby) => {
   const listItemElement = document.createElement('li');
   listItemElement.innerHTML = hobby;
   hobbiesElement.appendChild(listItemElement);
 });
 
+/* Places Lived DataList */
 const placesElement = document.getElementById('places-lived');
-myProfile.placesLived.forEach(place => {
+myProfile.placesLived.forEach((place) => {
   const dtElement = document.createElement('dt');
   dtElement.innerHTML = place.place;
   placesElement.appendChild(dtElement);
@@ -61,25 +71,3 @@ myProfile.placesLived.forEach(place => {
   ddElement.innerHTML = place.length;
   placesElement.appendChild(ddElement);
 });
-
-/* Populate Profile Object with placesLive objects */
-
-
-
-
-/* DOM Manipulation - Output */
-
-/* Name */
-
-/* Photo with attributes */
-
-
-/* Favorite Foods List*/
-
-
-/* Hobbies List */
-
-
-/* Places Lived DataList */
-
-
